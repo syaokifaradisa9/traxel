a149c57530bff470afd149976e83fa575f44a74e
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2023-09-27 12:30:01 +0800

    Penambahan Seeder Alat Terbaru

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -17,22 +17,29 @@
                   <thead>
                     <tr>
                       <th class="text-center" style="width: 50px">No.</th>
+                      <th class="text-center">Jumlah<br>Versi</th>
                       <th class="text-center">Alat Kesehatan</th>
                       <th class="text-center" style="width: 200px">Aksi</th>
                     </tr>
                   </thead>
                   <tbody>
                     @foreach ($alkes as $index => $value)
+                        @php
+                            $version_count = count($value->version);
+                        @endphp
                         <tr>
-                            <td class="text-center">{{ $index + 1 }}</td>
-                            <td class="align-middle">
+                            <td class="text-center" @if(!$version_count) text-danger @endif>{{ $index + 1 }}</td>
+                            <td class="text-center @if(!$version_count) text-danger @endif">
+                                {{ $version_count  }}
+                            </td>
+                            <td class="align-middle @if(!$version_count) text-danger @endif">
                                 {{ $value->name }}
                             </td>
-                            <td>
+                            <td class="text-center">
                                 <a href="{{ route('version.index', ['alkes_id' => $value->id]) }}" class="btn btn-primary">
                                     <i class="fas fa-search mr-1"></i>
                                     Detail
-                                </a>
+                                </a>                               
                             </td>
                         </tr>
                     @endforeach
